42678bf20591e8bd7c3d5e0070bf40e574b97d63
jmoody02 <jmoody@findaway.com>
2019-09-13 15:33:37 -0400

    wifi testing

--- sketch_sep10a/sketch_sep10a.ino
+++ sketch_sep10a/sketch_sep10a.ino
@@ -1,7 +1,21 @@
 #include <Servo.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "yourssid";
+const char* password = "yourpassword";
+
+const char* host = "wifitest.adafruit.com";
 
 void setup() {
   pinMode(0, OUTPUT);
+
+  //Wifi Connection
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 }
  
 void loop() {
@@ -9,4 +23,35 @@ void loop() {
   delay(500);
   digitalWrite(0, LOW);
   delay(500);
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/testwifi/index.html";
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(500);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
 }
