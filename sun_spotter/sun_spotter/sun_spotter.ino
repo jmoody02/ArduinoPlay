46529d581ae97af41f9503b61f16a14bdd1e5fd4
JMoody <jmoody@findaway.com>
2019-10-10 07:27:18 -0400

    Update sun_spotter.ino

--- sun_spotter/sun_spotter/sun_spotter.ino
+++ sun_spotter/sun_spotter/sun_spotter.ino
@@ -2,11 +2,15 @@
 #include <math.h>
 #include <Servo.h>
 #include <RTClib.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
 
 SunSpotter sunSpotter;
 Servo myservo2;
 Servo myservo16;
 RTC_PCF8523 rtc;
+Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
 
 void setup() {
   Serial.begin(115200);
@@ -18,10 +22,43 @@ void setup() {
     while (1);
   }
   Serial.println("Setup Complete");
+
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+  digitalWrite(0, HIGH);
 }
 
 void loop() {
 
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  mag.getEvent(&event);
+  
+  float Pi = 3.14159;
+  
+  // Calculate the angle of the vector y,x
+  float heading = (atan2(event.magnetic.y,event.magnetic.x) * 180) / Pi;
+  
+  // Normalize to 0-360
+  Serial.print("Compass Heading pre normalize: ");
+  Serial.println(heading);
+  if (heading < 0)
+  {
+    heading = 360 + heading;
+  }
+  if (heading > 5 && heading < 355) {
+      digitalWrite(0, LOW);
+    }
+    else {
+      digitalWrite(0, HIGH);
+    }
+  Serial.print("Compass Heading: ");
+  Serial.println(heading);
+  
   DateTime nowobj = rtc.now();
   time_t now = nowobj.unixtime()-14400;
   Serial.println(asctime(localtime(&now)));
@@ -33,20 +70,20 @@ void loop() {
   if (azimuth > 180) {
     azimuth = azimuth - 180;
   }
+  if (heading < 5) {
+    azimuth = azimuth - round(heading);
+  }
+  if (heading > 355) { 
+    azimuth = azimuth + round (360 - heading);
+  }
   Serial.println(azimuth);
   myservo16.write(azimuth);
-  Serial.println(sunSpotter.getAltitude());
   double altitude = round( sunSpotter.getAltitude() );
   if (altitude > 180) {
     altitude = altitude - 180;
   }
   Serial.println(altitude);
   myservo2.write(altitude);
-  for (int i=0; i<altitude; i++) {
-    digitalWrite(0, HIGH);
-    delay(200);
-    digitalWrite(0, LOW);
-    delay(200);
-  }
-  delay(2000);
+  
+  delay(100);
 }
