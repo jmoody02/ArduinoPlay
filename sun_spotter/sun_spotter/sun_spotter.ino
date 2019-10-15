9343763823109b7028bd1de533c34ec150436d3c
JMoody <jmoody@findaway.com>
2019-10-14 20:10:29 -0400

    Update sun_spotter.ino

--- sun_spotter/sun_spotter/sun_spotter.ino
+++ sun_spotter/sun_spotter/sun_spotter.ino
@@ -19,20 +19,21 @@ void setup() {
   generally_north = false;
   i = 0;
   heading = 0;
-  Serial.begin(115200);
+  /*Serial.begin(115200);*/
   myservo2.attach(2);
   myservo16.attach(16);
   pinMode(0, OUTPUT);
   if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
+    /*Serial.println("Couldn't find RTC");*/
     while (1);
   }
-  Serial.println("Setup Complete");
+  /*rtc.adjust(DateTime(2019, 10, 14, 11, 43, 0));*/
+  /*Serial.println("Setup Complete");*/
 
   if(!mag.begin())
   {
     /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    /*Serial.println("Ooops, no LSM303 detected ... Check your wiring!");*/
     while(1);
   }
   digitalWrite(0, HIGH);
@@ -52,32 +53,38 @@ void loop() {
     
     // Calculate the angle of the vector y,x
     heading = (atan2(event.magnetic.y,event.magnetic.x) * 180) / Pi;
+    /*-8 for magnetic declination*/
+    heading = heading -8;
     
     // Normalize to 0-360
-    Serial.print("Compass Heading pre normalize: ");
-    Serial.println(heading);
     if (heading < 0)
     {
       heading = 360 + heading;
     }
     if (heading > 5 && heading < 355) {
         digitalWrite(0, LOW);
+        generally_north = false;
       }
       else {
         digitalWrite(0, HIGH);
+        generally_north = true;
       }
-    Serial.print("Compass Heading: ");
-    Serial.println(heading);
+    /*Serial.print("Compass Heading: ");
+    Serial.println(heading);*/
   }
 
   if (i % 100 == 0) {
     DateTime nowobj = rtc.now();
     time_t now = nowobj.unixtime()-14400;
-    Serial.println(asctime(localtime(&now)));
+    /*Serial.println(asctime(localtime(&now)));*/
     
     sunSpotter.calculate(now);
     double azimuth = round( sunSpotter.getAzimuth() );
-    //adjust to my servo, which is 0 degrees for east
+    /*Serial.print("Initial Azimuth: ");
+    Serial.println(azimuth);*/
+    //adjust to my servo, which is 0 degrees for west, 180 is east
+    azimuth = azimuth * -1;
+    //west is coming in as -90, east as 90 now
     azimuth = azimuth - 90;
     if (azimuth > 180) {
       azimuth = azimuth - 180;
@@ -88,13 +95,15 @@ void loop() {
     if (heading > 355) { 
       azimuth = azimuth + round (360 - heading);
     }
-    Serial.println(azimuth);
+    /*Serial.print("Azimuth: ");
+    Serial.println(azimuth);*/
     myservo16.write(azimuth);
     double altitude = round( sunSpotter.getAltitude() );
     if (altitude > 180) {
       altitude = altitude - 180;
     }
-    Serial.println(altitude);
+    /*Serial.print("Altitude: ");
+    Serial.println(altitude);*/
     myservo2.write(altitude);
     nowobj = NULL;
   }
